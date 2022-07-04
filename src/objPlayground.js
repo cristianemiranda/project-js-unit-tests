7027a1783dc0d0de7abb9b87f9bce681f00b8697
lucas-da-silva <lucassouza00012345@gmail.com>
2022-07-04 19:05:11 -0300

    Add project files

---
+++ src/objPlayground.js
@@ -0,0 +1,57 @@
+/*
+  Implemente a função calculator que recebe dois números inteiros como parâmetro e retorna um objeto com as seguintes chaves:
+    - sum;
+    - mult;
+    - div;
+    - sub.
+  Para cada chave atribua como valor a operação correspondente à sua chave:
+    - sum: retorna o resultado da soma dos dois números;
+    - mult: retorna o resultado da multiplicação dos dois números;
+    - div: retorna o resultado da divisão dos dois números;
+    - sub: retorna o resultado da subtração dos dois números.
+
+  Os resultados das divisões devem sempre ser arredondados para baixo.
+
+  Parâmetros:
+  - Dois números inteiros.
+
+  Comportamento:
+  calculator(1, 2); // { sum: 3, mult: 2, div: 0, sub: -1 }
+
+  Depois de ter implementado a função calculator, desenvolva uma função que converta objetos em arrays, de chaves, valores ou ambos.
+  A sua função deve receber dois parâmetros:
+
+    * o primeiro parâmetro deve ser uma string que indica o tipo de conversão;
+    * o segundo parâmetro deve ser um objeto semelhante ao que é retornado pela função calculator que você acabou de desenvolver.
+
+  Parâmetros:
+  - Uma string que indica o tipo de conversão;
+  - Um objeto no formato { sum: 3, mult: 2, div: 0, sub: -1 };
+
+  Comportamento:
+  arrayGenerator('keys', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ 'sum', 'mult', 'div', 'sub' ]
+  arrayGenerator('values', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ 3, 2, 0, -1 ]
+  arrayGenerator('entries', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 0 ], [ 'sub', -1 ] ]
+*/
+
+const calculator = (number1, number2) => {
+  const soma = number1 + number2;
+  const multi = number1 * number2;
+  const divi = Math.floor(number1 / number2);
+  const subt = number1 - number2;
+  return { sum: soma, mult: multi, div: divi, sub: subt };
+};
+
+const arrayGenerator = (type, object) => {
+  let array = null;
+  if (type === 'keys') {
+    array = Object.keys(object);
+  } else if (type === 'values') {
+    array = Object.values(object);
+  } else {
+    array = Object.entries(object);
+  }
+  return array;
+};
+
+module.exports = { calculator, arrayGenerator };
